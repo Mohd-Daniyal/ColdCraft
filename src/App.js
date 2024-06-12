6a844832490d9edf1504aaab07874064db6f74dc
Mohd Daniyal <mohddaniyalh@gmail.com>
2024-06-12 18:47:32 +0530

    add api url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           ColdCraft{" "}
         </span>
       </h1>
-      <h3 className="mb-8 text-gray-400 text-xl font-bold"><i>Your Personal Cold Email Assistant</i></h3>
+      <h3 className="mb-8 text-gray-300 text-xl font-bold"><i>Your Personal Cold Email Assistant</i></h3>
       <div className="w-full sm:w-auto md:w-4/5 xl:w-5/6 p-8 md:p-10 lg:p-14 rounded-lg bg-black">
         <div className="max-w-xl w-full mx-auto space-y-12">
           <div className="lg:text-left text-center">
